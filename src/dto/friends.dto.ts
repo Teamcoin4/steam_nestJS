b0a952586f6c3525027d56622e266ccc5bb2e99b
yuchanKIM <kyc4537@naver.com>
2025-10-15 07:39:29 +0900

    friends-achievements-co

--- src/dto/friends.dto.ts
+++ src/dto/friends.dto.ts
@@ -4,7 +4,7 @@ export class FriendDto {
   id!: number;
   userId!: number;
   friendId!: string;
-  friend_since?: Date;
+  friend_since?: Date | null;
   created_at!: Date;
   updated_at!: Date;
 }
