5e1a16a9ea7d0f3c5075c1a2b97e0857e1680134
yuchanKIM <kyc4537@naver.com>
2025-10-18 01:21:24 +0900

    monitoring-fix

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MetricsService } from './common/metrics/metrics.service'; // 실제 경로에 맞게 수정
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,7 +9,7 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, MetricsService], // MetricsService 추가
     }).compile();
 
     appController = app.get<AppController>(AppController);
