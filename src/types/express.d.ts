85b7d28276373ad6283ab870bac1387af27c0eb1
Burinygang <dusdidrod987@naver.com>
2025-10-23 21:37:39 +0900

    modify/#2

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,4 +1,17 @@
 // src/types/express.d.ts
+import 'express';
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    user?: {
+      id: number;
+      steamId: string;
+      personaName?: string | null;
+      avatar?: string | null;
+    };
+  }
+}
+
 declare namespace Express {
   export interface Request {
     cookies?: Record<string, string>;
